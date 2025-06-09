33f22f48494fe83a9a215ea0751a836b25c03228
manan152003 <manan.gce21@sot.pdpu.ac.in>
2025-06-09 18:33:10 +0530

    checkking errors

--- src/services/categoryService.ts
+++ src/services/categoryService.ts
@@ -3,7 +3,7 @@ import type { Category } from '../types';
 const API_URL = import.meta.env.VITE_API_URL ;
 
 export const getCategories = async (): Promise<Category[]> => {
-  const response = await fetch(`${API_URL}/categories`);
+  const response = await fetch(`${API_URL}categories`);
   if (!response.ok) {
     throw new Error('Failed to fetch categories');
   }
